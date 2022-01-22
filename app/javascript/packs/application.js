b01e19782e4ed3030e6d6673198bee8c057196fd
sme777 <samson.petrosyan8@gmail.com>
2022-01-21 19:35:44 -0800

    start setting up development enviornment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,12 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+//= require foundation
+$(document).on('turbolinks:load', function() {
+    $(function(){ $(document).foundation(); });
+});
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
