7df9d902de0f689e240ee16df0956d56abae05ea
sme777 <samson.petrosyan8@gmail.com>
2022-01-23 10:23:27 -0800

    add sigup and login skeleton views

--- app/javascript/components/Login.jsx
+++ app/javascript/components/Login.jsx
@@ -6,7 +6,19 @@ const Login = () => {
     return (
         <div>
             <Navbar tab="login"/>
-            Login
+            <div className="login-container">
+                <form>
+                    <div className="sign-in-form">
+                        <h4 className="text-center">Sign In</h4>
+                        <label for="sign-in-form-username">Username</label>
+                        <input type="text" class="sign-in-form-username" id="sign-in-form-username"/>
+                        <label for="sign-in-form-password">Password</label>
+                        <input type="text" class="sign-in-form-password" id="sign-in-form-password"/>
+                        <button type="submit" class="sign-in-form-button">Sign In</button>
+                    </div>
+                </form>
+            </div>
+
             <Footer />       
         </div>
     )
